c5f29efea23ef6cafce23edf3e61253508607bc3
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2021-02-04 16:40:21 +0900

    add pwm code
    add pwm code

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  //analogWriteFrequency(3, 375000); // Teensy 3.0 pin 3 also changes to 375 kHz
+  analogWriteResolution(8);  // analogWrite value 0 to 255, or 256 for high
+  //analogWriteResolution(12);  // analogWrite value 0 to 4095, or 4096 for high
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(3, 128);
+}
