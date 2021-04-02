bf2494845b5ceef1764480a322c07ab009265436
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2021-04-02 11:09:53 +0900

    add test pin 29
    add test pin 29

--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,23 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  pinMode(3, OUTPUT);
+  //pinMode(3, OUTPUT);
+  pinMode(29, OUTPUT);
   //analogWriteFrequency(3, 375000); // Teensy 3.0 pin 3 also changes to 375 kHz
   //analogWriteFrequency(3, 1000000); // Teensy 4.1 pin 3 also changes to 1000 kHz
-  analogWriteFrequency(3, 50); // Teensy 4.1 pin 3 also changes to 50 kHz
+  //analogWriteFrequency(3, 50); // Teensy 4.1 pin 3 also changes to 50 kHz
+  analogWriteFrequency(29, 50); // Teensy 4.1 pin 3 also changes to 50 kHz
   //analogWriteResolution(8);  // analogWrite value 0 to 255, or 256 for high
   analogWriteResolution(15);  // analogWrite value 0 to 4095, or 4096 for high
 
-  //float pwm_width_percentage = 900.0 / 20000 * (1<<15);
-  float pwm_width_percentage = 2000.0 / 20000 * (1<<15);
+  float pwm_width_percentage = 900.0 / 20000 * (1<<15);
+  //float pwm_width_percentage = 2000.0 / 20000 * (1<<15);
   int pwm_width = int(pwm_width_percentage); //(2<<15);
   //int pwm_width = 2048;
   Serial.print("pwm_width: ");
   Serial.println( pwm_width);
 
-  analogWrite(3, pwm_width); // analogWrite value 0 to 4095, or 4096 for high
+  analogWrite(29, pwm_width); // analogWrite value 0 to 4095, or 4096 for high
 }
 
 void loop() {
